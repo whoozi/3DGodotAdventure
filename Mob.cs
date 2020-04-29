9f3f04947427455c3731b2cff7aaf19aaef7fa02
Alec White <whooziwhite@gmail.com>
2020-04-29 05:36:31 -0400

    Convert to mono; update to 3.2.2b1

---
+++ Mob.cs
@@ -0,0 +1,19 @@
+using Godot;
+
+public class Mob : KinematicBody
+{
+	const float Gravity = -9.8f;
+
+	[Export] protected float speed = 3f;
+
+	protected Vector3 velocity;
+
+	public override void _Process(float delta)
+	{
+		velocity.y += Gravity * delta;
+		MoveAndSlide(velocity, Vector3.Up, true);
+
+		if (IsOnFloor())
+			velocity.y = 0f;
+	}
+}
