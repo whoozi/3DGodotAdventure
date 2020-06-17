448bb3d95699ab5eb161b7971d64fa39837ae11e
Alec White <whooziwhite@gmail.com>
2020-06-17 04:50:53 -0400

    Begin hit raycast and implement weapon use queue

--- src/mobs/player/Player.cs
+++ src/mobs/player/Player.cs
@@ -2,23 +2,26 @@ using Godot;
 
 public class Player : Mob
 {
-	const float MouseSensitivity = 1f;
-	const float JoySensitivity = 2f;
+	const float MouseSensitivity = 0.4f;
+	const float JoySensitivity = 3f;
 
 	Vector2 mouseDelta, lookInput, moveInput;
 	float canvasWalkDegrees;
 	Vector3 lastOrigin, currentOrigin;
 	bool isOriginDirty;
-	Transform initialCamera;
+	Vector3 cameraOffset;
+	Weapon weapon;
+	bool isUseQueued;
 
+	// Nodes
 	Camera camera;
 	CanvasLayer canvas;
-	Weapon weapon;
+	RayCast ray;
 
-	private bool IsMouseCaptured() => Input.GetMouseMode() == Input.MouseMode.Captured;
+	bool IsMouseCaptured => Input.GetMouseMode() == Input.MouseMode.Captured;
 
 	// Used by the camera to reduce stutter caused by fixed physics update loop
-	private void RefreshOrigin()
+	void RefreshOrigin()
 	{
 		isOriginDirty = false;
 
@@ -26,14 +29,44 @@ public class Player : Mob
 		currentOrigin = Transform.origin;
 	}
 
+	void AnimateCanvas(float delta)
+	{
+		// Lerp canvas back to center
+		if (weapon != null)
+			canvas.Offset = new Vector2(
+				Mathf.Lerp(canvas.Offset.x - (200f * lookInput.x), 0, 20f * delta),
+				Mathf.Lerp(canvas.Offset.y, Mathf.Max(0, camera.RotationDegrees.x), 20f * delta)
+			);
+		else canvas.Offset = new Vector2(0f, 576f);
+
+		if (IsOnFloor())
+		{
+			// Do canvas "walking" animation
+			if (!moveInput.IsEqualApprox(Vector2.Zero))
+			{
+				canvasWalkDegrees += 6f * (velocity.Length() / speed) * delta;
+				canvas.Offset += new Vector2(Mathf.Sin(canvasWalkDegrees) * 10f, 2f + (Mathf.Cos(canvasWalkDegrees * 2f) * 2f)) * delta * 100f;
+			}
+		}
+	}
+
+	void ChangeWeapon(Weapon target)
+	{
+		if (weapon != null)
+			weapon.QueueFree();
+
+		weapon = target;
+		canvas.GetNode("Control").AddChild(weapon);
+		canvas.Offset = new Vector2(0f, 576f);
+	}
+
 	public override void _Ready()
 	{
 		camera = GetNode<Camera>("WorldCamera");
 		canvas = GetNode<CanvasLayer>("CanvasLayer");
-		// weapon = (Weapon)WeaponTable.sword.Instance();
-		// canvas.GetNode("Control").AddChild(weapon);
+		ray = camera.GetNode<RayCast>("RayCast");
 
-		initialCamera = camera.Transform;
+		cameraOffset = camera.Transform.origin;
 		camera.SetAsToplevel(true);
 
 		Input.SetMouseMode(Input.MouseMode.Captured);
@@ -61,54 +94,47 @@ public class Player : Mob
 		if (isOriginDirty)
 			RefreshOrigin();
 
-		lookInput = mouseDelta * MouseSensitivity * (GetViewport().Size * 0.0001f);
-		lookInput.y *= GetViewport().Size.x / GetViewport().Size.y;
+		lookInput = mouseDelta * (GetViewport().Size / OS.GetScreenSize()) * MouseSensitivity / 100f;
 		mouseDelta = Vector2.Zero;
 
 		lookInput += new Vector2(
 			Input.GetActionStrength("look_right") - Input.GetActionStrength("look_left"),
 			Input.GetActionStrength("look_down") - Input.GetActionStrength("look_up")
-		) * JoySensitivity * delta * 100f;
+		) * JoySensitivity * delta;
 
 		// Rotate player and camera
-		Rotate(Vector3.Up, Mathf.Deg2Rad(-lookInput.x));
-		camera.Rotation = new Vector3(Mathf.Clamp(camera.Rotation.x - Mathf.Deg2Rad(lookInput.y), -75, 75), Rotation.y, 0f);
+		Rotate(Vector3.Up, -lookInput.x);
+		camera.Rotation = new Vector3(Mathf.Clamp(camera.Rotation.x - lookInput.y, Mathf.Deg2Rad(-75f), Mathf.Deg2Rad(75f)), Rotation.y, 0f);
 		// Position camera; apply physics interpolation fraction to reduce stutter caused by fixed physics loop
-		camera.Translation = lastOrigin + ((currentOrigin - lastOrigin) * Engine.GetPhysicsInterpolationFraction()) + initialCamera.origin;
+		camera.Translation = lastOrigin + ((currentOrigin - lastOrigin) * Engine.GetPhysicsInterpolationFraction()) + cameraOffset;
 
-		// Lerp canvas back to center
-		if (weapon != null)
-			canvas.Offset = new Vector2(
-				Mathf.Lerp(canvas.Offset.x - lookInput.x, 0, 20f * delta),
-				Mathf.Lerp(canvas.Offset.y, Mathf.Max(0, camera.RotationDegrees.x), 20f * delta)
-			);
-		else canvas.Offset = new Vector2(0f, 576f);
+		AnimateCanvas(delta);
 
 		if (IsOnFloor())
 		{
-			// Do canvas "walking" animation
-			if (!moveInput.IsEqualApprox(Vector2.Zero))
+			if (weapon != null)
 			{
-				canvasWalkDegrees += 6f * (velocity.Length() / speed) * delta;
-				canvas.Offset += new Vector2(Mathf.Sin(canvasWalkDegrees) * 10f, 2f + (Mathf.Cos(canvasWalkDegrees * 2f) * 2f)) * delta * 100f;
+				if (!isUseQueued)
+				{
+					if (Input.IsActionJustPressed("attack"))
+						weapon.AttemptUse(ray, ref isUseQueued);
+				}
+				else weapon.AttemptUse(ray, ref isUseQueued);
 			}
-
-			if (weapon != null & Input.IsActionJustPressed("attack"))
-				weapon.Use();
 		}
 	}
 
 	public override void _UnhandledInput(InputEvent @event)
 	{
 		if (@event is InputEventMouseMotion motion)
-			if (IsMouseCaptured())
+			if (IsMouseCaptured)
 				mouseDelta = motion.Relative;
 
 		if (@event is InputEventKey key && key.Pressed)
 			switch ((KeyList)key.Scancode)
 			{
 				case KeyList.Escape:
-					Input.SetMouseMode(IsMouseCaptured() ? Input.MouseMode.Visible : Input.MouseMode.Captured);
+					Input.SetMouseMode(IsMouseCaptured ? Input.MouseMode.Visible : Input.MouseMode.Captured);
 					break;
 				case KeyList.F11:
 					OS.WindowFullscreen = !OS.WindowFullscreen;
@@ -117,16 +143,12 @@ public class Player : Mob
 					GetTree().ReloadCurrentScene();
 					break;
 				case KeyList.Key1:
-					if (weapon != null) weapon.QueueFree();
-					weapon = (Weapon)WeaponTable.sword.Instance();
-					canvas.GetNode("Control").AddChild(weapon);
-					canvas.Offset = new Vector2(0f, 576f);
+					if (weapon?.CanUse ?? true)
+						ChangeWeapon((Weapon)WeaponTable.sword.Instance());
 					break;
 				case KeyList.Key2:
-					if (weapon != null) weapon.QueueFree();
-					weapon = (Weapon)WeaponTable.axe.Instance();
-					canvas.GetNode("Control").AddChild(weapon);
-					canvas.Offset = new Vector2(0f, 576f);
+					if (weapon?.CanUse ?? true)
+						ChangeWeapon((Weapon)WeaponTable.axe.Instance());
 					break;
 			}
 	}
--- src/weapons/Weapon.cs
+++ src/weapons/Weapon.cs
@@ -1,8 +1,9 @@
 using Godot;
+using static Godot.GD;
 
 public class Weapon : Sprite
 {
-	public AnimationPlayer anim;
+	AnimationPlayer anim;
 
 	public override void _Ready()
 	{
@@ -15,14 +16,25 @@ public class Weapon : Sprite
 			anim.Play("idle");
 	}
 
-	public void Use()
+	public virtual bool CanUse => anim.CurrentAnimation != "swing_left";
+
+	public virtual bool CanBeQueued => !CanUse && anim.CurrentAnimationPosition / anim.CurrentAnimationLength > 0.5f;
+
+	public virtual void AttemptUse(RayCast ray, ref bool queueUse)
 	{
+		if (!CanUse)
+		{
+			queueUse = CanBeQueued;
+			return;
+		}
+
+		queueUse = false;
 		anim.Play("swing_left");
 
-		if (anim.CurrentAnimationPosition / anim.CurrentAnimationLength > 0.5f)
-		{
-			anim.ClearQueue();
-			anim.Queue("swing_left");
-		}
+		ray.CastTo = Vector3.Forward * 2f;
+		ray.ForceRaycastUpdate();
+
+		if (ray.IsColliding())
+			Print((ray.GetCollider() as Node).Name, ray.GetCollisionPoint());
 	}
 }
